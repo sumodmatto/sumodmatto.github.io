73bac59b507eeb7ba1fa5afbae6c20ab25e23a68
sumodmatto <calpissoda333@gmail.com>
2024-07-15 13:16:37 +0900

    Add Contact Page.

--- docs/components/header.js
+++ docs/components/header.js
@@ -7,6 +7,7 @@ class AppHeader extends HTMLElement {
               <li><a href="/index.html">Home</a></li>
               <li><a href="/blog/index.html">Blog</a></li>
               <li><a href="/app/index.html">App</a></li>
+              <li><a href="/contact/index.html">Contact</a></li>
             </ul>
           </nav>
         </header>
